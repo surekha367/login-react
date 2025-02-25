29e182c90f34b44f2dd33ef9adcf90ac189197ba
surekha367 <surekhakarasudula367@gmail.com>
2025-02-25 18:21:27 +0530

    added icons

---
+++ src/component/signin.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./signin.css";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    console.log("Remember Me:", rememberMe);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="left-container">
+        <img className="logo-image" src="logo.png" alt="BidGenie Logo" />
+        <div className="login-form-container">
+          <form className="login-form" onSubmit={handleSubmit}>
+            <h1>Welcome Back</h1>
+            <p>Log in by entering your credentials below.</p>
+            <div className="input-container">
+              <div class="input-with-icon">
+                <img src="mail.svg" alt="email-icon" />
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div class="input-with-icon">
+                <img src="password_lock.svg" alt="password-icon" />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="remember-me-forgot-password">
+                <label className="remember-me">
+                  <input
+                    type="checkbox"
+                    checked={rememberMe}
+                    onChange={() => setRememberMe(!rememberMe)}
+                  />
+                  Remember Me
+                </label>
+                <a href="#">Forgot Password?</a>
+              </div>
+              <button type="submit">Sign In</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="right-container"></div>
+    </div>
+  );
+}
+
+export default LoginPage;
